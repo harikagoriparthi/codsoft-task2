8b77f99a07275a5b8976949d969cc5c064337189
harikagoriparthi <155529099+harikagoriparthi@users.noreply.github.com>
2024-04-19 17:32:49 +0530

    Add files via upload

---
+++ Landing Page/script.js
@@ -0,0 +1,10 @@
+function imgSlider(anything)
+{
+   // document.getElementById("h2").innerHTML='dguyfsdhbvhsdkgv';
+    document.querySelector('.starbucks').src = anything;
+}
+function changeCircleColor(color)
+{
+    const circle =document.querySelector('.circle');
+    circle.style.background =color;
+}
